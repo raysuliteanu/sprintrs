b124667a290b8874178ed22dc265cb0158f96912
Ray Suliteanu <raysuliteanu@gmail.com>
2024-09-26 16:54:03 -0700

    get deserialization working

--- src/model.rs
+++ src/model.rs
@@ -1,53 +1,149 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct StartSpringIoModel {
+    #[serde(rename(deserialize = "_links"), skip)]
     links: Link,
     dependencies: Dependency,
+    #[serde(rename(deserialize = "type"), skip)]
     project_type: Project,
     packaging: Packaging,
     java_version: JavaVersion,
     language: Language,
     boot_version: BootVersion,
-    group_id: Option<String>,
-    artifact_id: Option<String>,
-    version: Option<String>,
+    group_id: Option<TypeAndDefault>,
+    artifact_id: Option<TypeAndDefault>,
+    version: Option<TypeAndDefault>,
+    name: Option<TypeAndDefault>,
+    description: Option<TypeAndDefault>,
+    package_name: Option<TypeAndDefault>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Link {}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
+struct Dependency {
+    #[serde(rename(deserialize = "type"))]
+    widget_type: String,
+    values: Vec<DependencyValue>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
+struct DependencyValue {
+    id: String,
     name: String,
     description: String,
-    package_name: String,
+    #[serde(rename(deserialize = "_links"), skip)]
+    links: Link,
+    #[serde(default)]
+    version_range: String,
 }
 
-#[derive(Debug, Deserialize)]
-struct Link {
-    content: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct Dependency {
-    content: String,
-}
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 struct Project {
-    content: String,
+    #[serde(rename(deserialize = "type"))]
+    action_type: String,
+    default: String,
+    values: Vec<ProjectType>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
+struct ProjectType {
+    id: String,
+    name: String,
+    description: String,
+    action: String,
+    tags: Vec<(String, String)>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 struct Packaging {
-    content: String,
+    #[serde(rename(deserialize = "type"))]
+    action_type: String,
+    default: String,
+    values: Vec<IdAndName>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 struct JavaVersion {
-    content: String,
+    #[serde(rename(deserialize = "type"))]
+    widget_type: String,
+    default: String,
+    values: Vec<IdAndName>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 struct Language {
-    content: String,
+    #[serde(rename(deserialize = "type"))]
+    widget_type: String,
+    default: String,
+    values: Vec<IdAndName>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
 struct BootVersion {
-    content: String,
+    #[serde(rename(deserialize = "type"))]
+    widget_type: String,
+    default: String,
+    values: Vec<IdAndName>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
+struct IdAndName {
+    id: String,
+    name: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(default)]
+struct TypeAndDefault {
+    #[serde(rename(deserialize = "type"))]
+    default_type: String,
+    #[serde(rename(deserialize = "default"))]
+    default_value: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io;
+
+    #[test]
+    fn deserialize_trivial() {
+        let json = r#"
+{ 
+    "_links": {}, 
+    "dependencies": {}, 
+    "type": {},
+    "packaging": {},
+    "javaVersion": {},
+    "language": {},
+    "bootVersion": {}
+}
+        "#;
+
+        let model = serde_json::from_str::<StartSpringIoModel>(json);
+        if model.is_err() {
+            println!("{:#?}", model);
+        }
+        assert!(model.is_ok());
+    }
+
+    #[test]
+    fn deserialize_full() {
+        let json = include_str!("../start.spring.io.json");
+        let model = serde_json::from_str::<StartSpringIoModel>(json);
+        assert!(model.is_ok());
+    }
 }
